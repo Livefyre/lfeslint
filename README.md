4c08502a5cd2c9724409e6d6a420cd68414ea98a
Ross Pfahler <ross.pfahler@gmail.com>
2015-11-25 16:51:20 -0800

    Refactor plugin to manually invoke eslint.
    No more use of standard-engine. Now supports varying by env, auto
    fix, formatters.

--- README.md
+++ README.md
@@ -4,9 +4,9 @@
 Linter for Livefyre Javascript projects
 
 ## Install
-npm install git+ssh://git@github.com/Livefyre/lfeslint.git
+Drop it into your ``package.json``
 
-or drop it into your ``package.json``
+or install via ``npm install git+ssh://git@github.com/Livefyre/lfeslint.git``
 
 ## Usage
 Running ``lfeslint`` will run the linter recurisvely in the current directory. It will skip certain directories by default.
@@ -14,6 +14,17 @@ Running ``lfeslint`` will run the linter recurisvely in the current directory. I
 To run on a certain path
 ```bash
 lfeslint ./src
+```
+
+### Command line options
+```bash
+-e  --env       Environment prefixes: browser, mocha, node, commonjs, es6 (defaults to all)
+-F  --fix       Automatically fix some issues
+-f  --formatter Output results as different format types
+-g  --global    Declare global variable
+-v  --version   Show current version
+-h, --help      Show usage information
+-i, --ignore    Ignore certain paths
 ```
 
 ### Ignoring Files
@@ -32,16 +43,29 @@ To add more directories to ignore, place the following in your package.json
 ```
 
 ### Hiding Warnings
-This project uses [`standard-engine](https://github.com/Flet/standard-engine), which in turn uses
-[`eslint`](http://eslint.org/). All of the eslint directives will work - for example: `/*eslint-disable*/`
+This project uses [`ESlint`](http://eslint.org/). All of the ESlint directives will work - for example: `/*eslint-disable*/`
 
-### Defining Globals in a project's  `package.json`
+### Defining Globals in a project's package.json
 Certain projects will need globals present to operate correctly.
 
-```js
+```json
 {
   "lfeslint": {
     "global": ["var1", "Q"]
   }
 }
 ```
+
+### Configuring environments
+A complete list of available environments can be found in the [`Specifying Environments`](http://eslint.org/docs/user-guide/configuring#specifying-environments) section of the ESLint docs.
+
+```json
+{
+  "lfeslint": {
+    "env": ["mocha", "es6"]
+  }
+}
+```
+
+### Choosing a formatter
+A complete list of available formatters can be found in the [`ESLint Formatters`](http://eslint.org/docs/user-guide/formatters/) section of the ESLint docs
--- index.js
+++ index.js
@@ -1,4 +1,78 @@
 #!/usr/bin/env node
 
-var opts = require('./options.js');
-require('standard-engine').cli(opts);
+/**
+ * Simple CLI + setup for running eslint.
+ * Owes love to https://github.com/Flet/standard-engine/blob/master/bin/cmd.js
+ */
+
+var CLIEngine = require('eslint').CLIEngine;
+var deepFillIn = require('mout/object/deepFillIn');
+var minimist = require('minimist');
+var multiline = require('multiline');
+var pkgConfig = require('pkg-config');
+
+var opts = {
+  version: require('./package.json').version,
+  cmd: 'lfeslint'
+};
+
+var argv = minimist(process.argv.slice(2), {
+  alias: {
+    env: 'e',
+    formatter: 'f',
+    fix: 'F',
+    global: 'g',
+    help: 'h',
+    ignore: 'i',
+    version: 'v'
+  },
+  boolean: [
+    'help',
+    'fix',
+    'version'
+  ],
+  string: [
+    'formatter'
+  ]
+});
+
+var perProjectConfig = pkgConfig('lfeslint') || {};
+argv = deepFillIn(argv, perProjectConfig);
+
+if (argv.help) {
+  console.log(multiline.stripIndent(function () {
+    /*
+      Usage:
+          %s <flags> [FILES...]
+          If FILES is omitted, then all JavaScript source files (*.js, *.jsx) in the current
+          working directory are checked, recursively.
+          Certain paths (node_modules/, .git/, coverage/, *.min.js, bundle.js) are
+          automatically ignored.
+      Flags:
+          -e  --env       Environment prefixes: browser, mocha, node, commonjs, es6 (defaults to all)
+          -F  --fix       Automatically fix some issues
+          -f  --formatter Output results as different format types
+          -g  --global    Declare global variable
+          -v  --version   Show current version
+          -h, --help      Show usage information
+          -i, --ignore    Ignore certain paths
+    */
+  }), opts.cmd);
+  process.exit(0);
+}
+
+if (argv.version) {
+  console.log(opts.version);
+  process.exit(0);
+}
+
+var cli = new CLIEngine({
+  extensions: ['.js', '.jsx'],
+  envs: argv.env && argv.env.length ? argv.env : ['browser', 'mocha', 'node', 'commonjs', 'es6'],
+  ignorePattern: ['node_modules/', '.git/', 'converage/', '*.min.js', 'dist/'].concat(argv.ignore || [])
+});
+
+var report = cli.executeOnFiles(argv._.length ? argv._ : ['./']);
+
+var formatter = cli.getFormatter(argv.formatter || 'stylish');
+console.log(formatter(report.results));
